4844dd5a56f8f66de7cf778ce6df71a896cd8572
swemarx <swemarx@aypee.net>
2020-11-04 13:43:48 +0100

    Seem to work okish

--- main.go
+++ main.go
@@ -7,31 +7,37 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const (
 	DEFAULT_HTTP_PORT      int    = 80
-	DEFAULT_HTTP_URI       string = "/health"
+	DEFAULT_HTTP_URI       string = "/health/"
 	DEFAULT_REDIS_ENDPOINT string = "127.0.0.1:6379"
 	REFRESH_INTERVAL_MS    int    = 1000 // Milliseconds
+	HTTP_RETURN_CODE_BAD   int    = 400  // Bad request
 	HTTP_RETURN_CODE_ERROR int    = 410  // Gone!
 )
 
+type RedisInstance struct {
+	endpoint  string
+	client    *redis.Client
+	isHealthy bool
+}
+
 var (
-	isEndpointHealthy = false
+	httpUri   string
+	instances []*RedisInstance
 )
 
 func main() {
 	var (
 		httpPort        int
-		httpUri         string
 		refreshInterval int
-		redisEndpoint   string
 		redisPassword   string
 		err             error
 		ctx             = context.Background()
-		rdb             *redis.Client
 	)
 
 	// Set variables from env
@@ -67,60 +73,83 @@ func main() {
 	}
 	fmt.Printf("[info] will perform healthchecks every %d ms\n", refreshInterval)
 
-	if envRedisEndpoint := os.Getenv("REDIS_ENDPOINT"); envRedisEndpoint != "" {
-		redisEndpoint = envRedisEndpoint
+	if envRedisEndpoints := os.Getenv("REDIS_ENDPOINTS"); envRedisEndpoints != "" {
+		fmt.Println("REDIS_ENDPOINTS:")
+		for _, e := range strings.Split(envRedisEndpoints, " ") {
+			fmt.Printf("- %s\n", e)
+			i := &RedisInstance{endpoint: e}
+			instances = append(instances, i)
+		}
 	} else {
-		redisEndpoint = DEFAULT_REDIS_ENDPOINT
+		instances = append(instances, &RedisInstance{endpoint: DEFAULT_REDIS_ENDPOINT})
 	}
-	fmt.Printf("[info] will check %s\n", redisEndpoint)
 
 	if envRedisPassword := os.Getenv("REDIS_PASSWORD"); envRedisPassword != "" {
 		redisPassword = envRedisPassword
-		fmt.Printf("[info] will authenticate using given password\n")
+		fmt.Println("[info] will authenticate using given password")
+	} else {
+		redisPassword = ""
+		fmt.Println("[info] will not authenticate")
 	}
 
-	// Init redis-client
-	if len(redisPassword) > 0 {
-		rdb = redis.NewClient(&redis.Options{
-			Addr:     redisEndpoint,
+	// Init redis-clients
+	for _, i := range instances {
+		i.client = redis.NewClient(&redis.Options{
+			Addr:     i.endpoint,
 			Password: redisPassword,
 			DB:       0,
-		})
-	} else {
-		rdb = redis.NewClient(&redis.Options{
-			Addr: redisEndpoint,
-			DB:   0,
 		})
 	}
 
 	// Listen in the background
 	go func(socket string) {
 		fmt.Printf("[info] now running!\n")
-		http.HandleFunc("/healthz", httpHandler)
+		http.HandleFunc(httpUri, httpHandler)
 		if err = http.ListenAndServe(socket, nil); err != nil {
 			fmt.Printf("[err] %s\n", err)
 			os.Exit(1)
 		}
 	}(":" + strconv.Itoa(httpPort))
 
+	//
 	// Mainloop
+	//
 	for {
-		time.Sleep(time.Duration(refreshInterval) * time.Millisecond)
-		pong, err := rdb.Ping(ctx).Result()
-		if pong != "PONG" || err != nil {
-			isEndpointHealthy = false
-		} else {
-			isEndpointHealthy = true
+		for _, i := range instances {
+			pong, err := i.client.Ping(ctx).Result()
+			if pong != "PONG" || err != nil {
+				fmt.Printf("[warn] %s\n", err)
+				i.isHealthy = false
+			} else {
+				i.isHealthy = true
+			}
 		}
+		time.Sleep(time.Duration(refreshInterval) * time.Millisecond)
 	}
 }
 
-// Handler returns 200 by default
+// Handler returns 200 by default. Kubernetes liveness/readiness checks only care about the http returncode, not the text.
 func httpHandler(w http.ResponseWriter, r *http.Request) {
-	if !isEndpointHealthy {
+	idxstr := strings.TrimPrefix(r.URL.Path, httpUri)
+	idx, err := strconv.Atoi(idxstr)
+	if err != nil {
+		fmt.Println("[error] Could not convert requested index into integer")
+		w.WriteHeader(HTTP_RETURN_CODE_BAD)
+		fmt.Fprintf(w, "BAD REQUEST\n")
+		return
+	}
+	l := len(instances)
+	if idx < 0 || idx > l-1 {
+		fmt.Println("[error] Invalid index requested")
+		w.WriteHeader(HTTP_RETURN_CODE_BAD)
+		fmt.Fprintf(w, "BAD REQUEST\n")
+		return
+	}
+	instance := instances[idx]
+	if !instance.isHealthy {
 		w.WriteHeader(HTTP_RETURN_CODE_ERROR)
-		fmt.Fprintf(w, "ERROR\n")
+		fmt.Fprintf(w, "%s is UNHEALTHY\n", instance.endpoint)
 	} else {
-		fmt.Fprintf(w, "OK\n")
+		fmt.Fprintf(w, "%s is HEALTHY\n", instance.endpoint)
 	}
 }
